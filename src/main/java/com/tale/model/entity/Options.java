43106b48da994f10caf79447451751391c34ff20
DionQiu <m18759158365@163.com>
2018-04-05 17:29:15 +0800

    first commit

---
+++ src/main/java/com/tale/model/entity/Options.java
@@ -0,0 +1,27 @@
+package com.tale.model.entity;
+
+import com.blade.jdbc.annotation.Table;
+import com.blade.jdbc.core.ActiveRecord;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 配置选项
+ *
+ * @author biezhi
+ */
+@Data
+@Table(value = "t_options", pk = "name")
+@EqualsAndHashCode(callSuper = false)
+public class Options extends ActiveRecord {
+
+    // 配置名称
+    private String name;
+
+    // 配置值
+    private String value;
+
+    // 配置描述
+    private String description;
+
+}
